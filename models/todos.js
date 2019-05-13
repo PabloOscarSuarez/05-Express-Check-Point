cb738d58ae1257285ca941d9020f2ff1f797fcc9
PabloOscarSuarez <pablooscarsuarez@gmail.com>
2019-05-12 23:14:07 -0300

    archivos necesario para el check

---
+++ models/todos.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var tasks = {}; // acá vamos a guardar nuestras personas y tareas
+
+module.exports = {
+  reset: function () {
+    tasks = {}; // esta función ya esta armada :D
+  },
+  // ==== COMPLETEN LAS SIGUIENTES FUNCIONES (vean los test de `model.js`) =====
+  listPeople: function () {
+    // devuelve un arreglo de personas con tareas
+  },
+  add: function (name, task) {
+    // guarda una tarea para una persona en particular
+  }
+  // etc.
+};
